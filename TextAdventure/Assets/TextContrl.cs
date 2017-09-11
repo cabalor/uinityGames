88d0a079651ac8f7236af51905d56df74eae89a9
Cab <cabalor@gmail.com>
2017-09-11 19:52:50 +0200

    newGame

---
+++ TextAdventure/Assets/TextContrl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextContrl : MonoBehaviour {
+
+
+	public Text text;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+		text.text = "Hell world";
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (Input.GetKeyDown (KeyCode.Space)) {
+			text.text = "halo halo";
+		}
+
+
+
+	}
+}
