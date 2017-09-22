008a82ae35a1a2b0ea9c3868208d1582cfe5d80d
Cab <cabalor@gmail.com>
2017-09-23 00:06:34 +0200

    new

---
+++ NumsUI/Assets/lvlMng.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lvlMng : MonoBehaviour {
+
+	public void Load(string lvl){
+
+		Debug.Log ("lvl loaded " + lvl);
+
+	}
+
+	public void quit(){
+		Debug.Log ("game quited");
+	}
+
+
+
+}
