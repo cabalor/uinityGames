6de43a956b8749f9f93f30094580a4b805f84797
Cab <cabalor@gmail.com>
2017-09-25 19:24:11 +0200

    changes

--- NumsUI/Assets/goBack.cs
+++ NumsUI/Assets/goBack.cs
@@ -1,19 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class lvlMng : MonoBehaviour {
+public class goBack : MonoBehaviour {
 
 	public void Load(string lvl){
-
+		
 		Debug.Log ("lvl loaded " + lvl);
-
+		
 		Application.LoadLevel(lvl);
-
+		
 	}
 
-	public void quit(){
-		Debug.Log ("game quited");
-	}
 
 
 
