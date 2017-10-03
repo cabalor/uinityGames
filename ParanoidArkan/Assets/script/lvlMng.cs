e17dcb0bb1521b5df779fe9010e9c0efe30f7456
Cab <cabalor@gmail.com>
2017-10-03 17:49:47 +0200

    new lvl

---
+++ ParanoidArkan/Assets/script/lvlMng.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lvlMng : MonoBehaviour {
+
+	public void Load(string lvl){
+
+		Debug.Log ("lvl loaded " + lvl);
+
+		Application.LoadLevel(lvl);
+
+	}
+
+	public void quit(){
+		Debug.Log ("game quited");
+		Application.Quit ();
+	}
+
+
+
+}
