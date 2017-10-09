5d50346a33b9983923793690ac095e4382966de0
Cab <cabalor@gmail.com>
2017-10-09 11:28:45 +0200

    col script

---
+++ ParanoidArkan/Assets/script/Colider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Colider : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D trigger){
+		print("something");
+	}
+
+	void OnCollisionEnter2D(Collision2D collision){
+
+		print ("something2");
+	}
+
+}
