99a891d38d6a03a77b64c1b05e2581338ccf4fc2
Cab <cabalor@gmail.com>
2017-10-10 16:07:20 +0200

    add

--- ParanoidArkan/Assets/script/Colider.cs
+++ ParanoidArkan/Assets/script/Colider.cs
@@ -3,8 +3,14 @@ using System.Collections;
 
 public class Colider : MonoBehaviour {
 
+	public lvlMng levelMng;
+
+
 	void OnTriggerEnter2D(Collider2D trigger){
 		print("something");
+		levelMng.Load("winer");
+
+
 	}
 
 	void OnCollisionEnter2D(Collision2D collision){
