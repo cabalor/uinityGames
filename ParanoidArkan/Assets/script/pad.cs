4f45876fda192368c285be6d8b27af6b983ef486
Cab <cabalor@gmail.com>
2017-10-13 19:39:50 +0200

    moving pad

---
+++ ParanoidArkan/Assets/script/pad.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pad : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 padPos = new Vector3 (3f, this.transform.position.y, 0f);
+
+
+		float mousePosition = Input.mousePosition.x / Screen.width * 16;
+
+		padPos.x = Mathf.Clamp(mousePosition+3, 3f, 18f);
+
+		print (mousePosition);
+
+		this.transform.position = padPos;
+
+
+	}
+}
