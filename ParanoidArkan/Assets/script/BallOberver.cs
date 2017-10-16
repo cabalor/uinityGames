55cf0044c8ecb8916360f171b77b8490098a8afa
Cab <cabalor@gmail.com>
2017-10-16 12:52:06 +0200

    ball lives

--- ParanoidArkan/Assets/script/BallOberver.cs
+++ ParanoidArkan/Assets/script/BallOberver.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class BallOberver : MonoBehaviour {
 
 	public pad pad;
-
+	private bool started = false;
 	private Vector3 padToBall;
 	// Use this for initialization
 	void Start () {
@@ -15,6 +15,15 @@ public class BallOberver : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		this.transform.position = pad.transform.position + padToBall;
+
+		if (started == false) {
+			this.transform.position = pad.transform.position + padToBall;
+		}
+		if(Input.GetMouseButtonDown(0)){
+			print ("mouse clicked");
+			this.rigidbody2D.velocity = new Vector2(2f , 15f);
+			started = true;
+		}
+
 	}
 }
