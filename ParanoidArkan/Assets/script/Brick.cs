c590876409a50f214c81b31f05da026962af0f43
Cab <cabalor@gmail.com>
2017-10-22 19:43:51 +0200

    bricks

---
+++ ParanoidArkan/Assets/script/Brick.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+
+
+	public int maxHp;
+	private int hits;
+
+
+	// Use this for initialization
+	void Start () {
+		hits = 0;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+
+
+	}
+	void OnCollisionEnter2D(Collision2D collision){
+
+			hits++;
+			print ("something2");
+
+	}
+
+
+}
