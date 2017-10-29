9bdd507ea3dd72918cd927bc2c1d5b7a0778afa4
Cab <cabalor@gmail.com>
2017-10-29 22:07:01 +0100

    cleaning up

--- ParanoidArkan/Assets/script/Brick.cs
+++ ParanoidArkan/Assets/script/Brick.cs
@@ -3,13 +3,12 @@ using System.Collections;
 
 public class Brick : MonoBehaviour {
 
-
-	public int maxHp;
+	
 	public Sprite[] sprites;
 
 	private int hits;
 	private lvlMng Manager;
-
+	private int maxHp;
 
 	
 	// Use this for initialization
@@ -24,8 +23,8 @@ public class Brick : MonoBehaviour {
 	
 	}
 	void OnCollisionExit2D(Collision2D collision){
-
-			hits++;
+		hits++;
+		maxHp = sprites.Length + 1; // maxhp equals number of spritees but we can change it to public and set it mnually
 		if (maxHp <= hits) {
 			Destroy (gameObject);
 		} else {
@@ -36,7 +35,10 @@ public class Brick : MonoBehaviour {
 
 	void LoadSprite(){
 		int arrIndx = hits - 1; // it should give you 0, coude arrays are numberd from 0, and when you want looad a sprite form array you have to begin from start of array.
+		if(sprites [arrIndx]){
 		this.GetComponent<SpriteRenderer> ().sprite = sprites [arrIndx];  //get component load a component in this case Sprite Renderer which is visible in gui. and this. is Brick, on particular Brick.
+
+		}
 	}
 
 	void WinTheGame(){
